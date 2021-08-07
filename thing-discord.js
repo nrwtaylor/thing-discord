a4516022b190c9823a5b05f7c949320329a9796b
Nick <nrwtaylor@gmail.com>
2021-08-07 15:32:46 -0700

    Discord integration test.

--- thing-discord.js
+++ thing-discord.js
@@ -1,53 +1,53 @@
-const Bot = require('keybase-bot')
+require("dotenv").config();
+const Discord = require("discord.js");
+const { Client, Intents } = require("discord.js");
 
-const bot = new Bot()
+const bot = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
-const username = 'abot'
+const TOKEN = process.env.TOKEN;
 
-// Put your paper key here.
-// It will look something like this.
-// But is not this.
-const paperkey = 'merp'
+bot.login(TOKEN);
 
 // Use for debugging.
-const var_dump = require('var_dump')
+const var_dump = require("var_dump");
 
 // Use Gearman to provide the stack connector.
-var gearmanode = require('gearmanode');
+var gearmanode = require("gearmanode");
 
+const client = gearmanode.client();
 
-bot
-  .init(username, paperkey, {verbose: false})
-  .then(() => {
-    console.log(`Your bot is initialized. It is logged in as ${bot.myInfo().username}`)
+bot.on("ready", () => {
+  console.log('Bot ' + bot.user.username + ' started.');
+});
 
-    const channel = {name: 'kbot,' + bot.myInfo().username, public: false, topicType: 'chat'}
+client.on('disconnect', message => {
+    client.close();
+    console.log('Bot ' + bot.user.username + ' disconnected.');
+    process.exit(1);
+});
 
-    // Reply to incoming traffic on all channels
-    const onMessage = message => {
+bot.on("message", (discordMessage) => {
+  // Is this a message from a bot?
+  if (discordMessage.author.bot) return;
 
-    // Some local logging/debugging.
-    // Uncomment to use.
-    //var_dump(message)
+  // Get the from, to and subject from the Keybase message.
+  var from = discordMessage.channelId;
+  var to = discordMessage.author.id;
+  var subject = discordMessage.content;
+  //console.log(message);
+  console.log('Heard, "' + subject + '"');
 
-    stations =  message.channel.name.split(",")
+  // dev display return from Discord.
+  // console.log(discordMessage);
 
-    const channel = message.channel
-    console.log('message received.')
+  match = false;
 
-    // https://www.npmjs.com/package/gearmanode
-    var client = gearmanode.client();
-
-    // Get the from, to and subject from the Keybase message.
-    var from = message.channel.name
-    var to =  message.sender.uid
-    var subject = message.content.text.body
-
-    console.log('Heard, "' + subject + '"')
-
-    // This suggests this is a DM so don't need
-    // to watch for a keyword
-    match = false
+  // dev code here to determine whether this is a two person
+  // channel.
+  /*
+// How many folk are in the channel?
     if (stations.length == 1) {
       var to =  message.channel.name
     }
@@ -55,81 +55,75 @@ bot
     if (stations.length == 2) {
       match = true;
     }
+*/
 
-    if (subject.toLowerCase().includes('ednabot')) {
-      match = true;
-    }
+  // Check if the message mentions the bot.
+  if (discordMessage.mentions.has(bot.user)) {
+    console.log("Saw the bot mentioned " + bot.user.id);
+    match = true;
+  }
 
-    if (subject.toLowerCase().includes('edna')) {
-      match = true;
-    }
+  // Add a list of words the bot should be responsive to.
+  if (subject.toLowerCase().includes("ednabot")) {
+    match = true;
+  }
 
-    if (subject.toLowerCase().includes('control')) {
-      match = true;
-    }
+  if (subject.toLowerCase().includes("edna")) {
+    match = true;
+  }
 
-    // For debuging/testing.
-    // Uncomment.
-    //console.log(to);
-    //console.log(from);
-    //console.log(subject);
-    //console.log(match);
+  if (subject.toLowerCase().includes("control")) {
+    match = true;
+  }
 
-    if (match == false) {return;}
+  if (match == false) {
+    return;
+  }
 
-    var arr = {"from":from,"to":to,"subject":subject}
-    var datagram = JSON.stringify(arr) 
+  var arr = { from: from, to: to, subject: subject };
+  var datagram = JSON.stringify(arr);
 
-    try {
-    var job = client.submitJob('call_agent', datagram);
-    }
+  try {
+    var job = client.submitJob("call_agent", datagram);
+  } catch (e) {
+    console.log(e);
 
-    catch (e) {
-      console.log(e);
+    var sms = "quiet";
+    var message = "Quietness. Just quietness.";
+  }
 
-      var sms = "quiet"
-      var message = "Quietness. Just quietness."
-    }
-
-    job.on('workData', function(data) {
-
+  job.on("workData", function (data) {
     // Uncomment for debugging/testing.
     //    console.log('WORK_DATA >>> ' + data);
+  });
 
-    });
+  job.on("complete", function () {
+    // Create a fallback message.
+    // Which says 'sms'.
+    sms = "sms";
+    message = "sms";
 
-    job.on('complete', function() {
+    try {
+      var thing_report = JSON.parse(job.response);
+      var sms = thing_report.sms;
+      var message = thing_report.message;
+    } catch (e) {
+      console.log(e);
 
-        // Create a fallback message.
-        // Which says 'sms'.
-        sms = "sms"
-        message = "sms"
-
-        try {
-          var thing_report = JSON.parse(job.response);
-          var sms = thing_report.sms
-          var message = thing_report.message
-        }
-
-        catch (e) {
-          console.log(e);
-
-          var sms = "quiet"
-          var message = "Quietness. Just quietness."
-        }
-
-        console.log(sms);
-        console.log(message);
-
-        bot.chat.send(channel, {body: sms})
-
-        client.close();
-      });
+      var sms = "quiet";
+      var message = "Quietness. Just quietness.";
     }
 
-    bot.chat.watchAllChannelsForNewMessages(onMessage)
+    console.log(sms);
+    console.log(message);
 
-  })
-  .catch(error => {
-    console.log(error)
-  })
+    // Respond to the channel with the sms
+    // channel response.
+    discordMessage.channel.send(sms);
+
+    // dev exploring ways to respond.
+    // discordMessage.reply(sms);
+    // message.lineReply(sms); //Line (Inline) Reply with mention
+    // message.lineReplyNoMention(`My name is ${client.user.username}`); //L
+  });
+});
